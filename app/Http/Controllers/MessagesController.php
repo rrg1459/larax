1e5e696891f7d6e2ef3236929f9e7c42980fccf9
Rafael <rrg1459@hotmail.com>
2018-07-08 11:16:18 -0400

    clase # 11

---
+++ app/Http/Controllers/MessagesController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Message;
+use Illuminate\Http\Request;
+
+class MessagesController extends Controller
+{
+    //
+    public function show(Message $message)
+    {
+    	return view('messages.show', [
+    		'message' => $message,
+    	]);
+    }
+}
