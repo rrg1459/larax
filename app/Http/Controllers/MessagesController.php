44bc1ec9c35cf2cc3926d55f956812b10420af4b
Rafael <rrg1459@hotmail.com>
2018-07-08 11:50:04 -0400

    clase # 12

--- app/Http/Controllers/MessagesController.php
+++ app/Http/Controllers/MessagesController.php
@@ -14,4 +14,11 @@ class MessagesController extends Controller
     		'message' => $message,
     	]);
     }
+
+    public function create(Request $request)
+    {
+   	dd($request->all());
+    	
+    	return 'Created!';
+    }
 }
