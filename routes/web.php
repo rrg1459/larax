a77c1164b5c2f79ffddac540c691db4eaa2f0b1a
Rafael <rrg1459@hotmail.com>
2018-07-10 06:50:39 -0400

    clase # 19

--- routes/web.php
+++ routes/web.php
@@ -18,3 +18,5 @@ Route::get('/messages/{message}', 'MessagesController@show');
 Route::post('/messages/create', 'MessagesController@create')->middleware('auth');
 
 Auth::routes();
+
+Route::get('/{username}', 'UsersController@show');
