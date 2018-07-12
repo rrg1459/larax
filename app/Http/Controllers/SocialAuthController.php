4f422ed917abadfe88bc9ff51eff9d018e75aa77
Rafael <rrg1459@hotmail.com>
2018-07-12 12:53:12 -0400

    clase # 25

--- app/Http/Controllers/SocialAuthController.php
+++ app/Http/Controllers/SocialAuthController.php
@@ -22,6 +22,15 @@ class SocialAuthController extends Controller
 
 //    	dd($user);
 
+        $existing = User::whereHas('socialProfiles', function ($query) use ($user) {
+            $query->where('social_id', $user->id);
+        })->first();
+
+        if ($existing !== null) {
+            auth()->login($existing);
+
+            return redirect('/');
+        }
 
     	session()->flash('facebookUser', $user);
     	return view('users.facebook', [
